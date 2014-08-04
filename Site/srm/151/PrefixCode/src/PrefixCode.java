692f2bb7a8bfa65f406de25450ded6aa7609828e
Patrick <pmonkelban@gmail.com>
2014-08-04 16:44:08 -0400

    Posted PrefixCode

--- Site/srm/151/PrefixCode/src/PrefixCode.java
+++ Site/srm/151/PrefixCode/src/PrefixCode.java
@@ -1,19 +1,23 @@
-public class PrefixCode {
+/*
+TopCoder
+Single Round Match: 151
+Division: 2
+Level: 1
+Points: 250
+Description: http://community.topcoder.com/stat?c=problem_statement&pm=1700
+ */
 
-    public static void main(String[] args) {
-        System.out.println(new PrefixCode()
-                .isOne(new String[]{"6G9Lnpzw", "kA", "SyW9fFaF", "k",
-                        "SyW9fFa", "6G", "6", "SyW9f"}));
-    }
+public class PrefixCode {
 
     public String isOne(String[] words) {
 
         for (int i = 0; i < words.length - 1; i++) {
             for (int j = i + 1; j < words.length; j++) {
+
                 if (words[i].startsWith(words[j])) {
                     return ("No, " + j);
                 } else if (words[j].startsWith(words[i])) {
-                    return "No, " + i;
+                    return ("No, " + i);
                 }
             }
         }
