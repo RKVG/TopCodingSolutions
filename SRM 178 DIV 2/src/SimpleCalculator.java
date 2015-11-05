5dcfcd6aa1357fd06bfe857abd109f0c85b5998c
Patrick Monkelban <pmonkelban@gmail.com>
2015-11-05 17:20:31 -0500

    Solved SimpleCalculator

---
+++ SRM 178 DIV 2/src/SimpleCalculator.java
@@ -0,0 +1,19 @@
+public class SimpleCalculator {
+	
+	public int calculate(String input) {
+
+		String[] ints = input.split("[+|\\-|\\*|/]");
+
+		char op = input.charAt(ints[0].length());
+
+		int i1 = Integer.parseInt(ints[0]);
+		int i2 = Integer.parseInt(ints[1]);
+
+		if ('+' == op) return i1 + i2;
+		if ('-' == op) return i1 - i2;
+		if ('*' == op) return i1 * i2;
+		return i1 / i2;
+
+	}
+
+}
